00e7ac71d82bb2b331ad74bbed61a0aafbf46742
yusrililhm <yusrililham62@gmail.com>
2024-04-25 06:39:12 +0700

    add: app configuration

--- infra/config/config.go
+++ infra/config/config.go
@@ -2,12 +2,35 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
-func LoadEnv()  {
+type appConfig struct {
+	AppPort    string
+	DBUser     string
+	DBHost     string
+	DBPort     string
+	DBName     string
+	DBPassword string
+	DBDialect  string
+}
+
+func LoadEnv() {
 	if err := godotenv.Load(".env"); err != nil {
-		log.Printf("error while load .env: %s\n", err.Error()) 
+		log.Printf("error while load .env: %s\n", err.Error())
+	}
+}
+
+func AppConfig() *appConfig {
+	return &appConfig{
+		AppPort:    os.Getenv("APP_PORT"),
+		DBHost:     os.Getenv("DB_HOST"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBDialect:  os.Getenv("DB_DIALECT"),
 	}
 }
