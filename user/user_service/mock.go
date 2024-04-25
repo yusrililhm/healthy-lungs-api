225e122df3ce4b82d3425601866bcc996397631c
yusrililhm <yusrililham62@gmail.com>
2024-04-25 15:28:53 +0700

    add: user domain

---
+++ user/user_service/mock.go
@@ -0,0 +1,47 @@
+package user_service
+
+import (
+	"expert_systems_api/dto"
+	"expert_systems_api/pkg/exception"
+	"expert_systems_api/pkg/helper"
+)
+
+type userServiceMock struct {
+}
+
+var (
+	ChangePassword func(userId int, payload *dto.UserChangePassword) (*helper.HTTPResponse, exception.Exception)
+	Modify         func(userId int, payload *dto.UserModifyPayload) (*helper.HTTPResponse, exception.Exception)
+	Profile        func(userId int) (*helper.HTTPResponse, exception.Exception)
+	SignIn         func(payload *dto.UserSignInPayload) (*helper.HTTPResponse, exception.Exception)
+	SignUp         func(payload *dto.UserSignUpPayload) (*helper.HTTPResponse, exception.Exception)
+)
+
+func NewUserServiceMock() UserService {
+	return &userServiceMock{}
+}
+
+// ChangePassword implements UserService.
+func (u *userServiceMock) ChangePassword(userId int, payload *dto.UserChangePassword) (*helper.HTTPResponse, exception.Exception) {
+	return ChangePassword(userId, payload)
+}
+
+// Modify implements UserService.
+func (u *userServiceMock) Modify(userId int, payload *dto.UserModifyPayload) (*helper.HTTPResponse, exception.Exception) {
+	return Modify(userId, payload)
+}
+
+// Profile implements UserService.
+func (u *userServiceMock) Profile(userId int) (*helper.HTTPResponse, exception.Exception) {
+	return Profile(userId)
+}
+
+// SignIn implements UserService.
+func (u *userServiceMock) SignIn(payload *dto.UserSignInPayload) (*helper.HTTPResponse, exception.Exception) {
+	return SignIn(payload)
+}
+
+// SignUp implements UserService.
+func (u *userServiceMock) SignUp(payload *dto.UserSignUpPayload) (*helper.HTTPResponse, exception.Exception) {
+	return SignUp(payload)
+}
