19e12bfcb7b4500a4f0c2fe2e70086ed302d8c68
yusrililhm <yusrililham62@gmail.com>
2024-04-27 21:59:53 +0700

    disease_service: add fetch method

--- disease/disease_service/service.go
+++ disease/disease_service/service.go
@@ -1,16 +1,38 @@
 package disease_service
 
-import "expert_systems_api/disease/disease_repo"
+import (
+	"expert_systems_api/disease/disease_repo"
+	"expert_systems_api/pkg/exception"
+	"expert_systems_api/pkg/helper"
+	"net/http"
+)
 
 type diseaseService struct {
 	dr disease_repo.DiseaseRepo
 }
 
 type DiseaseService interface {
+	Fetch() (*helper.HTTPResponse, exception.Exception)
 }
 
 func NewDiseaseService(dr disease_repo.DiseaseRepo) DiseaseService {
 	return &diseaseService{
 		dr: dr,
 	}
+}
+
+// Fetch implements DiseaseService.
+func (d *diseaseService) Fetch() (*helper.HTTPResponse, exception.Exception) {
+
+	diseases, err := d.dr.Fetch()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &helper.HTTPResponse{
+		Status:  http.StatusOK,
+		Message: "disease successfully fetched",
+		Data:    diseases,
+	}, nil
 }
