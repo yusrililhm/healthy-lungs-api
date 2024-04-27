b68df6f048c6f9eda25dae648e3b326713e6d65e
yusrililhm <yusrililham62@gmail.com>
2024-04-27 22:01:21 +0700

    infra_postgres: add initialize database function

--- infra/db/db.go
+++ infra/db/db.go
@@ -2,17 +2,68 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+
+	"expert_systems_api/infra/config"
 
 	_ "github.com/lib/pq"
 )
 
-func GetDbInstance() *sql.DB {
-	db, err := sql.Open("postgres", "host=localhost, port=5432, username=postgres, password=")
+var (
+	db  *sql.DB
+	err error
+)
+
+func handleDatabaseConnection() {
+
+	appConfig := config.AppConfig()
+
+	dataSourceName := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		appConfig.DBHost,
+		appConfig.DBPort,
+		appConfig.DBUser,
+		appConfig.DBPassword,
+		appConfig.DBName,
+	)
+
+	db, err = sql.Open("postgres", dataSourceName)
 
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatalf("error occured while trying to validate database arguments: %s\n", err.Error())
 	}
 
+	if err := db.Ping(); err != nil {
+		log.Fatalf("error occured while trying to connect to database: %s\n", err)
+	}
+}
+
+func handleRequestTables() {
+	const (
+		createTableUserQuery = `
+			CREATE TABLE IF NOT EXISTS user (
+				id serial primary key,
+				full_name varchar(60) not null,
+				email varchar(60) not null unique,
+				password varchar(text) not null,
+				role varchar(5) not null,
+				created_at timestamptz default = now(),
+				updated_at timestamptz default = now(),
+				deleted_at timestamptz
+			)
+		`
+	)
+
+	if _, err := db.Exec(createTableUserQuery); err != nil {
+		log.Fatalf("error while create table user: %s\n", err.Error())
+	}
+}
+
+func InitializeDatabase() {
+	handleDatabaseConnection()
+	handleRequestTables()
+}
+
+func GetDbInstance() *sql.DB {
 	return db
 }
