05c32649d4727bea27fc1f35c4c8e578c6710d55
yusrililhm <yusrililham62@gmail.com>
2024-04-27 21:59:34 +0700

    disease_handler: add fetch method

--- disease/disease_handler/handler.go
+++ disease/disease_handler/handler.go
@@ -1,16 +1,35 @@
 package disease_handler
 
-import "expert_systems_api/disease/disease_service"
+import (
+	"expert_systems_api/disease/disease_service"
+	"expert_systems_api/pkg/helper"
+	"net/http"
+)
 
 type diseaseHandler struct {
 	ds disease_service.DiseaseService
 }
 
 type DiseaseHandler interface {
+	Fetch(w http.ResponseWriter, r *http.Request)
 }
 
 func NewDiseaseHandler(ds disease_service.DiseaseService) DiseaseHandler {
 	return &diseaseHandler{
 		ds: ds,
 	}
+}
+
+// Fetch implements DiseaseHandler.
+func (d *diseaseHandler) Fetch(w http.ResponseWriter, r *http.Request) {
+	res, err := d.ds.Fetch()
+
+	if err != nil {
+		w.WriteHeader(err.Status())
+		w.Write(helper.ResponseJSON(err))
+		return
+	}
+
+	w.WriteHeader(res.Status)
+	w.Write(helper.ResponseJSON(res))
 }
