02b8b5506ead9ccea90beb80e7a7d42d8f3c2156
yusrililhm <yusrililham62@gmail.com>
2024-05-03 18:25:04 +0700

    [user entity] add: json tag and generate token method

--- entity/user.go
+++ entity/user.go
@@ -1,24 +1,35 @@
 package entity
 
 import (
+	"expert_systems_api/infra/config"
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	Id        int
-	FullName  string
-	Email     string
-	Password  string
-	Role      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt time.Time
+	Id        int       `json:"id"`
+	FullName  string    `json:"full_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
 }
 
 func (u *User) GenerateTokenString() string {
-	return ""
+
+	claims := jwt.MapClaims{
+		"email":      u.Email,
+		"expired_at": time.Now().Add(8 * time.Hour),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, _ := token.SignedString([]byte(config.AppConfig().JWTSecretKey))
+
+	return tokenString
 }
 
 func (u *User) CompareHashPassword(password string) bool {
