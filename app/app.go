9c66ad8ca184853a054206c64c4527b38580feb0
yusrililhm <yusrililham62@gmail.com>
2024-05-04 19:02:15 +0700

    [application] add: dependency injection for symtomp and disease, and route

--- app/app.go
+++ app/app.go
@@ -1,8 +1,14 @@
 package app
 
 import (
+	"expert_systems_api/disease/disease_handler"
+	"expert_systems_api/disease/disease_repo/disease_pg"
+	"expert_systems_api/disease/disease_service"
 	"expert_systems_api/infra/config"
 	"expert_systems_api/infra/db"
+	"expert_systems_api/symtomp/symtomp_handler"
+	"expert_systems_api/symtomp/symtomp_repo/symtomp_pg"
+	"expert_systems_api/symtomp/symtomp_service"
 	"expert_systems_api/user/user_handler"
 	"expert_systems_api/user/user_repo/user_pg"
 	"expert_systems_api/user/user_service"
@@ -28,13 +34,27 @@ func StartApplication() {
 	us := user_service.NewUserService(ur)
 	uh := user_handler.NewUserHandler(us)
 
+	sr := symtomp_pg.NewSymtompPg(db)
+	ss := symtomp_service.NewSymtompService(sr)
+	sh := symtomp_handler.NewSymtompHandler(ss)
+
+	dr := disease_pg.NewDiseasePg(db)
+	ds := disease_service.NewDiseaseService(dr)
+	dh := disease_handler.NewDiseaseHandler(ds)
+
 	r.Post("/user/signin", uh.SignIn)
 	r.Post("/user/signup", uh.SignUp)
 
 	r.Group(func(r chi.Router) {
 		r.Use(us.Authentication)
+
 		r.Get("/user", uh.Profile)
 		r.Patch("/user", uh.Modify)
+		r.Patch("/user/change-password", uh.ChangePassword)
+
+		r.Get("/symtomp", sh.Fetch)
+
+		r.Get("/disease", dh.Fetch)
 	})
 
 	log.Printf("[server is running] on port %s", config.AppConfig().AppPort)
