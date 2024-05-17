883f94232c3e17b26a701e98afe31def7fcf7e41
yusrililhm <yusrililham62@gmail.com>
2024-05-17 18:36:57 +0700

    [tag] fixed: json tag and valid tag

--- dto/user.go
+++ dto/user.go
@@ -3,35 +3,35 @@ package dto
 import "time"
 
 type UserSignInPayload struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `valid:"required~Email can't empty, email" json:"email"`
+	Password string `valid:"required~Password can't empty" json:"password"`
 }
 
 type UserSignUpPayload struct {
-	FullName string `json:"full_name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	FullName string `valid:"required~Full name can't empty" json:"full_name"`
+	Email    string `valid:"required~Email can't empty, email" json:"email"`
+	Password string `valid:"required~Password can't empty" json:"password"`
 }
 
 type UserModifyPayload struct {
-	FullName string `json:"full_name"`
-	Email    string `json:"email"`
+	FullName string `valid:"required~Full name can't empty" json:"full_name"`
+	Email    string `valid:"required~Email can't empty, email" json:"email"`
 }
 
 type UserData struct {
-	Id        int
-	FullName  string
-	Email     string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	Id        int       `json:"id"`
+	FullName  string    `json:"full_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type TokenString struct {
-	Token string `json:"token"`
+	Token string `valid:"required~ can't empty" json:"token"`
 }
 
 type UserChangePassword struct {
-	OldPassword        string `json:"old_password"`
-	NewPassword        string `json:"new_password"`
-	ConfirmNewPassword string `json:"confirm_new_password"`
+	OldPassword        string `valid:"required~Old password can't empty" json:"old_password"`
+	NewPassword        string `valid:"required~New password can't empty" json:"new_password"`
+	ConfirmNewPassword string `valid:"required~Confirm new password can't empty" json:"confirm_new_password"`
 }
