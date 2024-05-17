3eaaa3c97944cf9e6266aeb789273b3ae208e01c
yusrililhm <yusrililham62@gmail.com>
2024-05-17 18:30:15 +0700

    [validation] add: validation struct

--- pkg/helper/validation.go
+++ pkg/helper/validation.go
@@ -1,7 +1,16 @@
 package helper
 
-import "expert_systems_api/pkg/exception"
+import (
+	"expert_systems_api/pkg/exception"
+
+	"github.com/asaskevich/govalidator"
+)
 
 func ValidationStruct(s any) exception.Exception {
+
+	if _, err := govalidator.ValidateStruct(s); err != nil {
+		return exception.NewBadRequestError(err.Error())
+	}
+
 	return nil
 }
